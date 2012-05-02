8a473cd1434974fd5bffc20ec7be92337a6d806a
Michael Reuter <mareuternh@gmail.com>
2012-05-01 21:23:16 -0400

    Adding moon feature DB creation work.

---
+++ moon_db/make_catalog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import shapefile
+r = shapefile.Reader("MOON_nomenclature.shp")
+#sr = r.shapeRecords()
+#print sr[0].shape.points
+#print sr[0].record
+
+ofile = open("initial_cat.txt", "w+")
+for sr in r.shapeRecords():
+	vals = (str(0), sr.record[0], str(sr.record[5]), str(sr.record[6]), sr.record[7])
+	ofile.write("\t".join(vals)+"\n")
+	
+ofile.close()
+
+

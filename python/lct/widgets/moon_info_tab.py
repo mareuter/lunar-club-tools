1283dfaae6f31f854d1559c57e1822ba534039fc
Michael Reuter <mareuternh@gmail.com>
2012-06-01 22:13:12 -0400

    Refs #8. Creating object for tab layout.

---
+++ python/lct/widgets/moon_info_tab.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 1, 2012
+
+@author: Michael Reuter
+'''
+from PyQt4 import QtGui
+
+class MoonInfoTab(QtGui.QWidget):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, parent=None):
+        '''
+        Constructor
+        '''
+        super(MoonInfoTab, self).__init__(parent)

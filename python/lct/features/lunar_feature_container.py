3f3326cdbb54af38d757c67919796c986e4d52d2
Michael Reuter <mareuternh@gmail.com>
2012-06-21 17:20:18 -0400

    Refs #11. Enforcing the feature visibility criterion.

--- python/lct/features/lunar_feature_container.py
+++ python/lct/features/lunar_feature_container.py
@@ -15,6 +15,8 @@ class LunarFeatureContainer(object):
     This class is responsible for gathering and distributing the lunar 
     feature information.
     '''
+    DEBUG = True
+
 
     def __init__(self, dbname="lct/db/moon.db"):
         '''
@@ -53,9 +55,10 @@ class LunarFeatureContainer(object):
             feature = LunarFeature(row[NAME], row[LATITUDE], row[LONGITUDE],
                                    row[TYPE], row[D_LAT], row[D_LONG],
                                    row[LUNAR_CODE], row[LUNAR_CLUB_TYPE])
-            self.features[id(feature)] = feature
-            self.club_type.add(row[LUNAR_CLUB_TYPE])
-            self.feature_type.add(row[TYPE])
+            if LunarFeatureContainer.DEBUG or obs_info.moon_info.isVisible(feature):
+                self.features[id(feature)] = feature
+                self.club_type.add(row[LUNAR_CLUB_TYPE])
+                self.feature_type.add(row[TYPE])
 if __name__ == "__main__":
     lfc = LunarFeatureContainer("../db/moon.db")
     lfc.load()

74959ea5f6a7e2b7a99d4ec9512032164780ada5
Michael Reuter <mareuternh@gmail.com>
2012-07-06 09:19:11 -0400

    Adding lunar dome records.

--- moon_db/final_catalog.py
+++ moon_db/final_catalog.py
@@ -49,6 +49,13 @@ for line in cat_file:
         feature_dict[values[1]] = (code, otype)
 cat_file.close()
 
+def fix_longitude(longitude):
+    if 180.0 <= longitude < 360.0:
+        # West is negative
+        return longitude - 360.0
+    else:
+        return longitude
+
 # Open and read through the shapefile looking for the features in 
 # the previously filled dict
 import shapefile
@@ -61,11 +68,7 @@ for sr in r.shapeRecords():
         feature_dia = sr.record[DIAMETER]
         feature_lat = sr.record[CENTER_LAT]
         long = sr.record[CENTER_LONG]
-        if 180.0 <= long < 360.0:
-            # West is negative
-            feature_long = long - 360.0
-        else:
-            feature_long = long
+        feature_long = fix_longitude(long)
         feature_type = sr.record[FEATURE_TYPE].split(',')[0]
         feature_delta_lat = math.fabs(sr.record[MAX_LAT] - sr.record[MIN_LAT]) 
         feature_delta_long = math.fabs(sr.record[MAX_LONG] - sr.record[MIN_LONG])
@@ -77,12 +80,33 @@ for sr in r.shapeRecords():
             feature_lunar_club_type = LUNAR_CLUB_TYPES[feature_dict[feature_name][1]]
         except IndexError:
             feature_lunar_club_type = None
-        
+
         records.append((feature_name, feature_dia,
                        feature_lat, feature_long, 
                        feature_delta_lat, feature_delta_long,
                        feature_type, feature_quad_name, feature_quad_code,
                        feature_lunar_code, feature_lunar_club_type))
+
+# Adding extra records for lunar domes which do not exist in the shapefile
+# Feature diameter is set a longitudinal diameter. To find delta latitude and
+# delta longitude, the mean lunar radius of 1737.1 km was used and the given 
+# diameter of the feature was used.
+arago_alpha = ("Arago Alpha", 24.0, 7.466666, 21.416666, 0.4947, 0.7916, 
+               "Dome", "Julius Caesar", "LAC-60", "LunarII", None)
+arago_beta = ("Arago Beta", 23.29, 6.083333, 19.93333, 0.6597, 0.7682,
+              "Dome", "Julius Caesar", "LAC-60", "LunarII", None)
+cauchy_omega = ("Cauchy Omega", 9.6, 7.23333, 38.316666, 0.3166, 0.3166, 
+                "Dome", "Taruntius", "LAC-61", "LunarII", None)
+cauchy_tau = ("Cauchy Tau", 10.2, 7.53333, 36.73333, 0.3364, 0.3364, 
+              "Dome", "Taruntius", "LAC-61", "LunarII", None)
+kies_pi = ("Kies Pi", 13.0, -26.93333, -24.23333, 0.4288, 0.4288,
+           "Dome", "Pitatus", "LAC-94", "LunarII", None)
+
+records.append(arago_alpha)
+records.append(arago_beta)
+records.append(cauchy_omega)
+records.append(cauchy_tau)
+records.append(kies_pi)
 
 # Setup SQL table
 features_table = []

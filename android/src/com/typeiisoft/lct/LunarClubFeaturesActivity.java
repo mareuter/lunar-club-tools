ce0360a3bf28125baa75c855f9ee94d6b497e8a0
Michael Reuter <mareuternh@gmail.com>
2012-07-15 16:27:03 -0400

    Refs #6. Creating listview layout with empty textview.

--- android/src/com/typeiisoft/lct/LunarClubFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarClubFeaturesActivity.java
@@ -2,15 +2,11 @@ package com.typeiisoft.lct;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class LunarClubFeaturesActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-        TextView textview = new TextView(this);
-        textview.setText("This is the Lunar Club feature tab");
-        setContentView(textview);
+        setContentView(R.layout.lcfeatures);
 	}
 }
--- android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
@@ -2,15 +2,11 @@ package com.typeiisoft.lct;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class LunarTwoFeaturesActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-        TextView textview = new TextView(this);
-        textview.setText("This is the Lunar II features tab");
-        setContentView(textview);
+        setContentView(R.layout.l2features);
 	}
 }

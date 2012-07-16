83f0767c4f57a3e9c586213c2c4d2a742f2c1cf2
Michael Reuter <mareuternh@gmail.com>
2012-07-16 18:10:02 -0400

    Refs #6. Filling out the Lunar Club features tab with actual information.

--- android/src/com/typeiisoft/lct/LunarClubFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarClubFeaturesActivity.java
@@ -1,12 +1,22 @@
 package com.typeiisoft.lct;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
+import com.typeiisoft.lct.db.DataBaseHelper;
+import com.typeiisoft.lct.features.LcFeatureAdapter;
+import com.typeiisoft.lct.features.LunarFeature;
+
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class LunarClubFeaturesActivity extends Activity {
+public class LunarClubFeaturesActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.lcfeatures);
+        DataBaseHelper moonDB = new DataBaseHelper(this);
+        LcFeatureAdapter adapter = new LcFeatureAdapter(this, 
+        		(ArrayList<LunarFeature>) moonDB.getLunarClubFeatures());
+        setListAdapter(adapter);
 	}
 }

1e5173e61c57e7a825e91e21807876b5c97a2fdc
Michael Reuter <mareuternh@gmail.com>
2012-07-17 07:15:21 -0400

    Refs #6. Adding log message to check creation status.

--- android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
@@ -8,10 +8,14 @@ import com.typeiisoft.lct.features.LunarFeature;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LunarTwoFeaturesActivity extends ListActivity {
+	private final static String TAG = "LunarTwoFeaturesActivity";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "Creating tab.");
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.l2features);
         DataBaseHelper moonDB = new DataBaseHelper(this);

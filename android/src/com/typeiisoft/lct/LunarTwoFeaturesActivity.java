9d1b12997c5ae8904c939cd1d877cfb0418341d3
Michael Reuter <mareuternh@gmail.com>
2012-07-16 20:31:12 -0400

    Refs #6. Setting up the LunarII features tab.

--- android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
@@ -1,12 +1,22 @@
 package com.typeiisoft.lct;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
+import com.typeiisoft.lct.db.DataBaseHelper;
+import com.typeiisoft.lct.features.L2FeatureAdapter;
+import com.typeiisoft.lct.features.LunarFeature;
+
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class LunarTwoFeaturesActivity extends Activity {
+public class LunarTwoFeaturesActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.l2features);
+        DataBaseHelper moonDB = new DataBaseHelper(this);
+        L2FeatureAdapter adapter = new L2FeatureAdapter(this, 
+        		(ArrayList<LunarFeature>) moonDB.getLunarTwoFeatures());
+        setListAdapter(adapter);
 	}
 }

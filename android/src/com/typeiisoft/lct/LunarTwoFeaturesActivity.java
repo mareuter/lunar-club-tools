14452e565457195bb21f3de5dcbda95f28961617
Michael Reuter <mareuternh@gmail.com>
2012-07-22 09:55:36 -0400

    Refs #6. LunarII feature list has long click to show more info about feature.

--- android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
+++ android/src/com/typeiisoft/lct/LunarTwoFeaturesActivity.java
@@ -9,7 +9,19 @@ import com.typeiisoft.lct.features.LunarFeature;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.Toast;
 
+/**
+ * This class handles creating the ListView for the LunarII club features. 
+ * The item view is just the name of the feature. More information can be 
+ * had by performing a long click on an item.
+ * 
+ * @author Michael Reuter
+ *
+ */
 public class LunarTwoFeaturesActivity extends ListActivity {
 	private final static String TAG = "LunarTwoFeaturesActivity";
 	
@@ -22,5 +34,18 @@ public class LunarTwoFeaturesActivity extends ListActivity {
         L2FeatureAdapter adapter = new L2FeatureAdapter(this, 
         		(ArrayList<LunarFeature>) moonDB.getLunarTwoFeatures());
         setListAdapter(adapter);
+        
+        // Setup the long click listener to display more information
+        OnItemLongClickListener listener = new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				String text = parent.getItemAtPosition(position).toString();
+				Toast.makeText(LunarTwoFeaturesActivity.this, text, 
+						Toast.LENGTH_LONG).show();
+				return true;
+			}
+		};
+		this.getListView().setOnItemLongClickListener(listener);
 	}
 }

950c909a6a3130dac44438bb790d38ad742ecbf2
Michael Reuter <mareuternh@gmail.com>
2012-07-31 20:45:03 -0400

    Refs #5. Using the preferences to create the MoonInfo object.

--- android/src/com/typeiisoft/lct/MoonInfoActivity.java
+++ android/src/com/typeiisoft/lct/MoonInfoActivity.java
@@ -1,18 +1,39 @@
 package com.typeiisoft.lct;
 
+import com.typeiisoft.lct.utils.AppPreferences;
 import com.typeiisoft.lct.utils.MoonInfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+/**
+ * This class handles displaying the Moon information via an activity and 
+ * view.
+ * 
+ * @author Michael Reuter
+ *
+ */
 public class MoonInfoActivity extends Activity {
+	/** Logging tag. */
+	private static final String TAG = "MoonInfoActivity";
+	/** The application preferences. */
+	private AppPreferences appPrefs;
+	
+	/**
+	 * This function creates the MoonInfo activity.
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mooninfo);
 		
-		MoonInfo moonInfo = new MoonInfo();
+		this.appPrefs = new AppPreferences(this.getApplicationContext());
+		
+		MoonInfo moonInfo = new MoonInfo(appPrefs.getDateTime());
+		Log.i(TAG, "MoonInfo: " + moonInfo.toString());
+		
 		String[] dateTime = moonInfo.obsDateTime();
 		this.appendText(R.id.obsdate_label, dateTime[0]);
 		this.appendText(R.id.obstime_label, dateTime[1]);
